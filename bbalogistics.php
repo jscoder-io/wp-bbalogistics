0b28633d3a4480bf894f2152d3d44e2cb6d17d22
JSCoder <jscoder.io@gmail.com>
2016-07-12 07:49:28 +0700

    test

---
+++ bbalogistics.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: BBA Logistics
+ * Plugin URI: https://bbalogistics.com.au/
+ * Description: Multi Carrier Shipping and Logistics Technology able to seamlessly integrate into your Woo cart.
+ * Version: 1.0.0
+ * Author: BBA Logistics
+ * Author URI: https://bbalogistics.com.au/
+ *
+ * @package BBALogistics
+ * @category Core
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
